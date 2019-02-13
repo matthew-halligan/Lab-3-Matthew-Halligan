b510edbe480936e55328929bf5249f9ea616b05c
matthew-halligan <mjhallig@uvm.edu>
2019-02-12 21:00:37 -0500

    Addition of first resume

---
+++ resume.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Resume Template Exercise</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Robert Erickson">
+    <meta name="description" content="Trying to help students out with getting a resume online">
+
+    <link rel="stylesheet"
+          href="style.css"
+          media="screen">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="resume">
+<h1>Prepare a Resume web page</h1>
+
+<p>Getting that first job</p>
+<ol>
+    <li>Prepare a resume which is what this page is about.</li>
+    <li>Visit a career office (<a href='http://www.uvm.edu/~career/'>UVM Career Center</a>).</li>
+    <li>Get that Job!</li>
+</ol>
+<p>So now that you have a few classes under your belt its time to get a summer job in the  field of your major! Before you start contacting companies you need to have a resume. Now I firmly believe everyone's resume should reflect the persons personality. Now having said that I want to provide a template for you to have your resume posted on the web. The purpose of this resume template is to get you started. I hope you will design a really neat style sheet and email it to me so i can use it as an <a href="samples.html">example</a>!
+
+<p>You will need to understand a little html coding (try <a href="http://www.w3schools.com/html/default.asp">w3schools</a>, also check out their <a href="http://www.w3schools.com/css/default.asp">CSS</a> tutorials) but you should not have any trouble learning this. Look at the resume, View the source and save the source to your public_html folder. You can use any text editor, html editor or word processor to edit this template. I suggest you save the template as resume.html as it just makes sense to do so.</p>
+
+
+<h2>Three simple steps.</h2>
+<ol>
+    <li>Copy the html template: <a href="resume_template.html">Resume Template</a></li>
+    <li>Copy the css template: <a href="css_template.html">CSS Template</a></li>
+    <li><a href="change.html">Change the content</a></li>
+</ol>
+
+<p>It does include <a href="http://microformats.org/">MicroFormats</a> that allow other applications to make sense of your data (for example <a href="http://www.linkedin.com/">LinkedIn</a> uses micro-formats).</p>
+
+<h2>General Resume Concepts</h2>
+
+<p>Some pointers can be found at: <a href="http://www.uvm.edu/~career/?Page=resume.html&SM=jobsubmenu.html">uvm career services.</a></p>
+
+<p>If your GPA is less than 3.0 don't list it.
+
+<p>I have a list of classes that I have taken. This is to help you fill a piece of paper. As you gain experience this section can be eliminated. Listing your courses also helps to give you some common ground when you go for an interview as the person interviewing will often ask about a particular class. Never list a course number or title but a general title. For example if you have taken CS 265 (Computer Networks) you can list:
+
+<ul>
+    <li>Computer Networks
+    <li>Networks
+</ul>
+
+<p>but never list:
+
+<ul>
+    <li>CS 265
+    <li>CS 265 Computer Networks
+</ul>
+
+<p>The course number is irrelevant, it only means something to someone here at UVM. If you have the room I would put <b>Computer Networks</b> but computers would be assumed so if you need to save space you can get away with <b>Networks</b>.
+
+<p>Ok now a very important part of your resume is the work experience which should be ordered with most recent first. Having any job is always good to put down as it shows the employer that this person has had a job before. It is even better if the job happens to be in your field. I always recommend to students to get a summer job in their field starting after there first year of education! Sometimes that may not be possible because you just can't find one so try again your 2nd summer and by all means your 3rd summer better be a job in your field. If you can take the summer after you graduate off and start your new job in September if you can get away with it :)
+
+<p>Ok now any job you have says something and you have to be sure you put it down. If you were a waiter/waitress put down how many tables you had and how many meals you served a night (this shows how much stress you can handle and how much you can juggle around in your head). I see lots of resumes and I say the same thing to everyone, expand that description of that job! How many people did you work with or manage or deal with, did you work independently mowing lawns (i.e. the boss dropped you off and said mow these 10 lawns this morning) which would show a prospective employer that you can work on your own.
+
+<p>You should see the job block can be copied as many times as needed.
+
+<p>Interests always seem to show on a resume and I have been told this is to show the employer that you are a human being with varied interests, plus if the interviewer is interested in the same thing you can talk about it. The interview process is in it self a whole other matter. Keep in mind that while you are in sight of the interviewees you are being interviewed (when you walk away they may watch you walk across the parking lot to see your reactions). The best interviewers will get you to talk to them as if you were their best friend.
+
+<p>Well I guess that is about all I have to say, so good luck with your resume.
+
+</body>
+</html>
