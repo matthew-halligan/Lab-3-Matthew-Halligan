51a590f3ace44394cc55bbcbf104e746dc8a32cb
matthew-halligan <mjhallig@uvm.edu>
2019-02-13 11:31:56 -0500

    final

--- resume.php
+++ resume.php
@@ -9,21 +9,21 @@
     -->
 
     <!-- **** you need to put in y our own title below -->
-    <title>Your Name (this will show up as the google link)</title>
+    <title>Matt Halligan</title>
 
     <meta http-equiv="Content-Type" content="charset=utf-8" >
 
     <!-- **** you need to in your own description in the content below -->
-    <meta name="description" content="This is what shows up as a google description so sell yourself!" >
+    <meta name="description" content="Student Web developer with an interest in protecting your digital assets" >
 
     <!-- **** you need to in your name in the content below -->
-    <meta name="author" content="Your Name" >
+    <meta name="author" content="Matthew Halligan" >
 
     <!-- **** the href below is the name of your style sheet that will define the
     look for your resume. -->
 
 
-    <link href='css/rerickso/sample2.css' rel='stylesheet' type='text/css' media='screen' />
+    <link href='resume.css' rel='stylesheet' type='text/css' media='screen' />
 
     <link href='css/rerickso/print.css' rel='stylesheet' type='text/css' media='print' />
 </head>
@@ -34,15 +34,15 @@
     <section id="hcard-Robert-Erickson" class="vcard">
 
         <!-- ****  Of course link to your photo and change the alt description -->
-        <p id="myImage"><!-- placeholder to put image as background if you want to --></p>
+        <p id="myImage"> </p>
 
         <!-- ****  put in  your name and your url -->
-        <h1 id="name"><a class="url fn" href="http://www.uvm.edu/~rerickso">Robert Erickson</a></h1>
+        <h1 id="name"><a class="url fn" href="http://www.mjhallig.w3.uvm.edu">Matt Halligan</a></h1>
 
 
         <!-- ****  in all the following span tags put in your address info -->
         <p class="adr">
-            <span class="street-address">331 Votey</span>,
+            <span class="street-address">486 South Prospect Street</span>,
             <span class="locality">Burlington</span>,
             <span class="region">Vermont</span>,
             <span class="postal-code">05405</span>
@@ -54,18 +54,18 @@
 
             <!-- ****  notice you need to put your address in twice, once for the link and
             once to display -->
-            <span class="value"><a class="email" href="mailto:robert.erickson@uvm.edu">robert.erickson@uvm.edu</a></span>
+            <span class="value"><a class="email" href="mailto:Matthew.Halligan@uvm.edu">Matthew.Halligan@uvm.edu</a></span>
         </p>
 
         <!-- ****  type in your contact numbers, delete extra, copy if you need more -->
         <p class="tel">
             <span class="type">Cell: </span>
-            <span class="value">1.802.656.8137</span>
+            <span class="value">1.203.626.1748</span>
         </p>
 
         <p class="tel">
             <span class="type">Work: </span>
-            <span class="value">1-802-656-8137</span>
+            <span class="value">1.203.626.1748</span>
         </p>
 
     </section>  <!-- ends vcard -->
@@ -96,17 +96,20 @@
         <!-- Begin job -->
         <ol>
             <li class="job" id="coName">
-                <span class="dates">1991 - present</span>
-                <a href="http://www.uvm.edu" class="company">University of Vermont</a>,
+                <span class="dates">2015-2017</span>
+                <a href="http://www.uvm.edu" class="company">Powder Ridge Ski Resort</a>,
 
                 <!-- notice i change the google maps url. everyplace it has a & made it &amp; so it would
                 validate -->
                 <a href="http://maps.google.com/maps?q=+colchester+ave%2C+Burlington%2C+VT+05401%2C+United+States&amp;l=explore&amp;utm_campaign=en&amp;utm_medium=ha&amp;utm_source=en-ha-na-us-gns-lt&amp;utm_term=searchbox&amp;submit.x=116&amp;submit.y=23&amp;submit=Explore+this+place"
-                   class="location">Burlington, VT</a>
+                   class="location">Middletown, CT</a>
 
-                <span class="jobtitle">Senior Lecturer.</span>
+                <span class="jobtitle">Rental Technician</span>
 
-                <p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                <p class="description">As a rental technician I was responsible for communicating the process of fitting rental
+                equiptment, fixing or tuning skis to the customer.  After a piece of equipment went on the mountain 5 times it was
+                in need of being checked and tuned to the standards set by manufacturers. I was tasked with being sure that
+                equipment was being properly tuned and in working condition.</p>
             </li><!-- end of this job -->
 
 
@@ -115,11 +118,11 @@
 
 
             <!-- list web sites and projects here -->
-            <li class="job" id="workExamples"><span class="dates">1995 - present</span>
+            <li class="job" id="workExamples"><span class="dates">2018-Present</span>
                 Websites and Projects
                 <ul class="websites">
-                    <li><a href="http://www.yoursite.com">name of site</a> short description</li>
-                    <li><a href="http://www.yoursite.com">name of site</a> short description</li>
+                    <li><a href="https://www.halliganwebdevelopment.com">Halligan Web Development</a> My side business of building websites.</li>
+                    <li><a href="https://www.thesuperiorbarbershop.com">The Superior Barbershop</a> A project for the Superior Barbershop located in Wallingford,CT.</li>
                 </ul>
             </li>
             <!-- end web sites and projects -->
@@ -132,10 +135,10 @@
     <section id="skills">
         <h2>Skills</h2>
         <ul>
-            <li>HTML</li>
-            <li>CSS</li>
+            <li>Customer Relations</li>
+            <li>Python</li>
             <li>PHP</li>
-            <li>MySQL</li>
+            <li>CSS</li>
             <li>JavaScript</li>
         </ul>
     </section> <!-- ends skill sets -->
@@ -154,31 +157,31 @@
 
         <ol>
             <li class="school" id="uvm">
-                <span class="dates">2008 - 2012</span>
+                <span class="dates">2017-Present</span>
                 <a href="http://www.uvm.edu" class="institution">University of Vermont</a>,
                 <a href="http://maps.google.com/maps?q=+colchester+ave%2C+Burlington%2C+VT+05401%2C+United+States&amp;l=explore&amp;utm_campaign=en&amp;utm_medium=ha&amp;utm_source=en-ha-na-us-gns-lt&amp;utm_term=searchbox&amp;submit.x=116&amp;submit.y=23&amp;submit=Explore+this+place"
                    class="location">Burlington, VT</a>
 
-                <span class="MajorDegree">Your Major</span>
-                <span class="MinorDegree">Minor</span>
-                <span class="gpa">Cumulative GPA 4.0 (4.0)</span>
+                <span class="MajorDegree">Computer Science</span>
+                <span class="MinorDegree">Mathematics</span>
+                <span class="gpa">Cumulative GPA 3.8 (3.8)</span>
 
                 <!-- List the most important courses for your major that you have taken (or will take). This gives you some common ground when interviewing and something to talk about. list the NAME not the number. If there is a link for your course put it in but be sure the link will always
                 be available. -->
                 <h3 class="subtitle">Course of Study </h3>
                 <ul class="courses major">
                     <li><a  href="http://giraffe.uvm.edu/rg-bin/rg_section_data.cgi?200801+10783">MS Office Beyond the Basics</a></li>
-                    <li>Database Design</li>
-                    <li>Digital Revolution</li>
-                    <li>Human Factors</li>
-                    <li>Software Design</li>
+                    <li>Advanced Web Design</li>
+                    <li>Linear Algebra</li>
+                    <li>Discreet Structures</li>
+                    <li>Applied Probability</li>
                 </ul>
 
 
                 <ul class="courses minor">
-                    <li>Elements of Drawing</li>
-                    <li>Archery</li>
-                    <li>Marksmanship</li>
+                    <li>Calculus 1</li>
+                    <li>Calculus 2</li>
+                    <li>Linear Algebra</li>
                 </ul>
             </li> <!-- end school  -->
 
@@ -203,13 +206,14 @@
 
         <ol>
             <li class="job">
-                <span class="dates">beginning year - present</span>
-                <a href="http://www.uvm.edu" class="company">Company</a>,
-                <a href="http://maps.google.com/" class="location">City, State</a>
+                <span class="dates">2018-Present</span>
+                <a href="http://www.uvm.edu" class="company">University Of Vermont</a>,
+                <a href="http://maps.google.com/" class="location">Burlington, VT</a>
 
-                <span class="jobtitle">Job Title</span>
+                <span class="jobtitle">TA for Cybersecurity Defence</span>
 
-                <p class="description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                <p class="description">As a TA I am responsible for helping students through challenging portions of the class.
+                </p>
             </li><!-- end of this job -->
 
             <!-- past other jobs here descending by dates -->
@@ -223,7 +227,7 @@
     <section id="interests">
         <h2>Interests</h2>
         <ol>
-            <li class="int">what else do you like that makes you a well rounded person?</li>
+            <li class="int">In my free time I enjoy hiking or running my business.</li>
         </ol>
     </section> <!-- ends interests -->
 
